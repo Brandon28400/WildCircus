df43ec6de82b78b78878e6ade933b603a0ddd7aa
Brandon Rabour <brandonrabour@yahoo.fr>
2019-07-16 14:19:16 +0200

    add routing & navbar

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CompagnieComponent } from './compagnie/compagnie.component';
+import { TarifComponent } from './tarif/tarif.component';
+import { WildcircustourComponent } from './wildcircustour/wildcircustour.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'compagnie', component: CompagnieComponent },
+  { path: 'tarifs', component: TarifComponent },
+  { path: 'wild-circus-tour', component: WildcircustourComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
