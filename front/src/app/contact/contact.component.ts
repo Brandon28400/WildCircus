da7687a951659a652719ab491b326c898fc8982c
Brandon Rabour <brandonrabour@yahoo.fr>
2019-07-17 11:41:19 +0200

    back back and back

--- front/src/app/contact/contact.component.ts
+++ front/src/app/contact/contact.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Member } from '../member';
+import { SendmailService } from '../sendmail.service';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+ selector: 'app-contact',
+ templateUrl: './contact.component.html',
+ styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+ public visitor: Member;
+ private littleclient: HttpClient;
 
-  ngOnInit() {
-  }
+ // here i initialise the variable as the member class
+
+ public lastName: string = null;
+ public firstName: string = null;
+ public phoneNumber: string = null;
+ public email: string = null;
+ public comment: string = null;
+
+ constructor(private emailService: SendmailService) {
+   this.visitor = new Member();
+ }
+ submit() {
+   this.visitor.lastName = this.lastName;
+   this.visitor.firstName = this.firstName;
+   this.visitor.phoneNumber = this.phoneNumber;
+   this.visitor.email = this.email;
+   this.visitor.comment = this.comment;
+
+   this.emailService.sendmail(this.visitor).subscribe(
+     () => {
+       alert("L'email a bien été envoyé !");
+     }
+
+   );
+   console.log(this.visitor)
+ }
+
+ ngOnInit() {
+ }
 
 }
